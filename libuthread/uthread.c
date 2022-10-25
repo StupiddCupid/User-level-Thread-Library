963dc3d175b7d5ec9a7aeb6d1af9a0eb3acc60d5
Zifei Cheng <110521637+StupiddCupid@users.noreply.github.com>
2022-10-25 15:16:48 -0700

    update create() function

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -9,8 +9,16 @@
 #include "private.h"
 #include "uthread.h"
 
+typedef enum {
+	READY,
+	RUNNING,
+
+} state;
+
 struct uthread_tcb {
-	/* TODO Phase 2 */
+	void *context_ptr;
+	void *stack_ptr;
+	state thread_state;
 };
 
 struct uthread_tcb *uthread_current(void)
@@ -18,19 +26,49 @@ struct uthread_tcb *uthread_current(void)
 	/* TODO Phase 2/4 */
 }
 
+/*
+ * uthread_yield - Yield execution
+ *
+ * This function is to be called from the currently active and running thread in
+ * order to yield for other threads to execute.
+ */
 void uthread_yield(void)
 {
 	/* TODO Phase 2 */
 }
 
+/*
+ * uthread_exit - Exit from currently running thread
+ *
+ * This function is to be called from the currently active and running thread in
+ * order to finish its execution.
+ *
+ * This function shall never return.
+ */
 void uthread_exit(void)
 {
 	/* TODO Phase 2 */
 }
 
+
+/*
+ * uthread_create - Create a new thread
+ * @func: Function to be executed by the thread
+ * @arg: Argument to be passed to the thread
+ *
+ * This function creates a new thread running the function @func to which
+ * argument @arg is passed.
+ *
+ * Return: 0 in case of success, -1 in case of failure (e.g., memory allocation,
+ * context creation).
+ */
+
 int uthread_create(uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+	struct uthread_tcb *myThread = (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
+	myThread -> stack_ptr = uthread_ctx_alloc_stack();
+	myThread -> thread_state = READY;
+	uthread_ctx_init(myThread->context_ptr, myThread->stack_ptr, func, arg);
 }
 
 int uthread_run(uthread_func_t func, void *arg)
