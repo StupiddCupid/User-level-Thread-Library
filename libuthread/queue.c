ef99b8931f72e3253345c3a51c649a858e5330a1
Hugo <lminghao1@gmail.com>
2022-10-25 09:14:39 -0700

    working on queue.c

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -7,7 +7,7 @@
 struct queue 
 {
 	int length;
-    struct Node*front, *tail;
+    struct Node *front, *tail;
 };
 
 struct Node
@@ -44,6 +44,16 @@ int queue_destroy(queue_t queue)
     return 0;
 }
 
+/*
+ * queue_enqueue - Enqueue data item
+ * @queue: Queue in which to enqueue item
+ * @data: Address of data item to enqueue
+ *
+ * Enqueue the address contained in @data in the queue @queue.
+ *
+ * Return: -1 if @queue or @data are NULL, or in case of memory allocation error
+ * when enqueing. 0 if @data was successfully enqueued in @queue.
+ */
 int queue_enqueue(queue_t queue, void *data)
 {
     //if queue is null or data is null
@@ -64,22 +74,68 @@ int queue_enqueue(queue_t queue, void *data)
 	
 }
 
+/*
+ * queue_dequeue - Dequeue data item
+ * @queue: Queue in which to dequeue item
+ * @data: Address of data pointer where item is received
+ *
+ * Remove the oldest item of queue @queue and assign this item (the value of a
+ * pointer) to @data.
+ *
+ * Return: -1 if @queue or @data are NULL, or if the queue is empty. 0 if @data
+ * was set with the oldest item available in @queue.
+ */
 int queue_dequeue(queue_t queue, void **data)
 {
-	/* TODO Phase 1 */
+    if(queue == NULL || data == NULL || queue->length == 0) return -1;
+    struct Node *frontNode = queue->front;
+    queue->front = queue->front->next;
+    data = frontNode->key;
+    return 0;
 }
 
+/*
+ * queue_delete - Delete data item
+ * @queue: Queue in which to delete item
+ * @data: Data to delete
+ *
+ * Find in queue @queue, the first (ie oldest) item equal to @data and delete
+ * this item.
+ *
+ * Return: -1 if @queue or @data are NULL, of if @data was not found in the
+ * queue. 0 if @data was found and deleted from @queue.
+ */
 int queue_delete(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
 }
 
+/*
+ * queue_iterate - Iterate through a queue
+ * @queue: Queue to iterate through
+ * @func: Function to call on each queue item
+ *
+ * This function iterates through the items in the queue @queue, from the oldest
+ * item to the newest item, and calls the given callback function @func on each
+ * item. The callback function receives the current data item as parameter.
+ *
+ * Note that this function should be resistant to data items being deleted
+ * as part of the iteration (ie in @func).
+ *
+ * Return: -1 if @queue or @func are NULL, 0 otherwise.
+ */
 int queue_iterate(queue_t queue, queue_func_t func)
 {
-	/* TODO Phase 1 */
+	if(queue== NULL) return -1;
+    for(int i = 0; i < queue->length; i++) {
+        
+    }
+
+    return 0;
 }
 
 int queue_length(queue_t queue)
 {
-	/* TODO Phase 1 */
+    if (queue == NULL) return -1;
+    else return queue->length;
 }
