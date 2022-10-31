4532e7959deabc6a317055d84e66f43d722536fe
HugoLin-16 <76938794+HugoLin-16@users.noreply.github.com>
2022-10-31 10:51:23 -0700

    Update queue_tester_example.c

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -23,15 +23,39 @@ void test_create(void)
 	TEST_ASSERT(queue_create() != NULL);
 }
 
+void test_delete()
+{
+    printf("testing delete\n");
+    // int *ptr;
+
+    static int d1 = 1;
+    static int d2 = 2;
+    static int d3 = 3;
+    static int d4 = 4;
+    static int d5 = 5;
+	queue_t q;
+
+	q = queue_create();
+    printf("test delete\n\n");
+    queue_enqueue(q, &d1);
+    queue_enqueue(q, &d2);
+    queue_enqueue(q, &d3);
+    queue_enqueue(q, &d4);
+    queue_enqueue(q, &d5);
+    queue_delete(q, &d4);
+    TEST_ASSERT(queue_length(q) == 4);
+    printf("finished testing delete\n");
+}
+
 static void iterator_inc(queue_t q, void *data)
 {
-    //printf("in process of iteration\n");
+    // printf("in process of iteration\n");
     int *a = (int*)data;
-
+    
     if (*a == 42){
         //printf("there is a 42 in queue, deleting it\n");
         queue_delete(q, data);
-        //printf("deletion done\n");
+        printf("deletion done\n");
     }
     else
         *a += 1;
@@ -49,9 +73,9 @@ void test_iterator(void)
         queue_enqueue(q, &data[i]);
 
     /* Increment every item of the queue, delete item '42' */
-    //printf("testing iteration function\n");
+    printf("testing iteration function\n");
     queue_iterate(q, iterator_inc);
-    //printf("finished iteration\n");
+    printf("finished iteration\n");
     TEST_ASSERT(data[0] == 2);
     TEST_ASSERT(queue_length(q) == 9);
 }
@@ -92,8 +116,9 @@ void test_queue_simple(void)
 
 int main(void)
 {
-	test_create();
+	// test_create();
 	test_queue_simple();
     test_iterator();
+    test_delete();
 	return 0;
 }
