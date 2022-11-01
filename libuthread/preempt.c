c2f3820f3f7ddac3c31784928d8e3066c5872bfc
HugoLin-16 <76938794+HugoLin-16@users.noreply.github.com>
2022-11-01 03:03:59 -0700

    used preempt variable
    used preempt variable in function preempt_start to void compilation error

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,23 +15,48 @@
  */
 #define HZ 100
 
+/*
+ * preempt_disable - Disable preemption
+ */
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
 }
 
+/*
+ * preempt_enable - Enable preemption
+ */
 void preempt_enable(void)
 {
 	/* TODO Phase 4 */
 }
 
+/*
+ * preempt_start - Start thread preemption
+ * @preempt: Enable preemption if true
+ *
+ * Configure a timer that must fire a virtual alarm at a frequency of 100 Hz and
+ * setup a timer handler that forcefully yields the currently running thread.
+ *
+ * If @preempt is false, don't start preemption; all the other functions from
+ * the preemption API should then be ineffective.
+ */
 void preempt_start(bool preempt)
 {
+	if (preempt) printf("start thread preemption\n");
 	/* TODO Phase 4 */
 }
 
+/*
+ * preempt_stop - Stop thread preemption
+ *
+ * Restore previous timer configuration, and previous action associated to
+ * virtual alarm signals.
+ */
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
 }
+
+
 
