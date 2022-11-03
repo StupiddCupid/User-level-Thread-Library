3cd38a15a8df15a6eb9120ffccfc9afe90d954b0
Hugo <lminghao1@gmail.com>
2022-11-03 16:38:30 -0700

    check

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -42,16 +42,6 @@ int queue_destroy(queue_t queue)
     return 0;
 }
 
-/*
- * queue_enqueue - Enqueue data item
- * @queue: Queue in which to enqueue item
- * @data: Address of data item to enqueue
- *
- * Enqueue the address contained in @data in the queue @queue.
- *
- * Return: -1 if @queue or @data are NULL, or in case of memory allocation error
- * when enqueing. 0 if @data was successfully enqueued in @queue.
- */
 int queue_enqueue(queue_t queue, void *data)
 {
     //if queue is null or data is null
@@ -59,7 +49,7 @@ int queue_enqueue(queue_t queue, void *data)
 
     //create a new node, return -1 if fails to create new node
     struct Node* new_node = newNode(data);
-    if (new_node == NULL){ return -1; }
+    if (new_node == NULL) { return -1; }
 
     //add new node if queue is empty
     if (queue->length == 0){
@@ -84,17 +74,6 @@ int queue_enqueue(queue_t queue, void *data)
     return 0;
 }
 
-/*
- * queue_dequeue - Dequeue data item
- * @queue: Queue in which to dequeue item
- * @data: Address of data pointer where item is received
- *
- * Remove the oldest item of queue @queue and assign this item (the value of a
- * pointer) to @data.
- *
- * Return: -1 if @queue or @data are NULL, or if the queue is empty. 0 if @data
- * was set with the oldest item available in @queue.
- */
 int queue_dequeue(queue_t queue, void **data)
 {
     if(queue == NULL || data == NULL || queue->length == 0) return -1;
@@ -115,20 +94,10 @@ int queue_dequeue(queue_t queue, void **data)
     return 0;
 }
 
-/*
- * queue_delete - Delete data item
- * @queue: Queue in which to delete item
- * @data: Data to delete
- *
- * Find in queue @queue, the first (ie oldest) item equal to @data and delete
- * this item.
- *
- * Return: -1 if @queue or @data are NULL, of if @data was not found in the
- * queue. 0 if @data was found and deleted from @queue.
- */
 int queue_delete(queue_t queue, void *data)
 {
 	if (queue == NULL || data == NULL) return -1;
+
     struct Node *prev_node = queue->front, *curr_node = queue->front;
     int dataFound = 0;
 
@@ -163,24 +132,13 @@ int queue_delete(queue_t queue, void *data)
     else return 0;
 }
 
-/*
- * queue_iterate - Iterate through a queue
- * @queue: Queue to iterate through
- * @func: Function to call on each queue item
- *
- * This function iterates through the items in the queue @queue, from the oldest
- * item to the newest item, and calls the given callback function @func on each
- * item. The callback function receives the current data item as parameter.
- *
- * Note that this function should be resistant to data items being deleted
- * as part of the iteration (ie in @func).
- *
- * Return: -1 if @queue or @func are NULL, 0 otherwise.
- */
 int queue_iterate(queue_t queue, queue_func_t func)
 {
 	if(queue== NULL || func == NULL) return -1;
-    struct Node *curr_node = queue->front, *next_node = queue->front->next;
+
+    struct Node *curr_node = queue->front;
+    struct Node *next_node = queue->front->next;
+    
     while(curr_node != NULL) {
         func(queue, curr_node->key);
         if (next_node != NULL) {
