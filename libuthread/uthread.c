8b0ef8d590dc6f7ffab47ca310683256d32c825e
Hugo <lminghao1@gmail.com>
2022-11-03 17:17:28 -0700

    add comments

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -29,6 +29,7 @@ struct uthread_tcb {
 	state thread_state;
 };
 
+// deallocate uthrea_tcb 
 void clear_exited_queue()
 {
 	struct uthread_tcb *thread_to_free;
@@ -57,46 +58,60 @@ void uthread_yield(void)
 	current_thread->thread_state = READY;
 	queue_enqueue(Ready_Queue, (void*)current_thread);
 
-	//Get next thread 
+	//Get next available thread 
 	struct uthread_tcb *next_thread;
-	queue_dequeue(Ready_Queue, (void**)&next_thread);
-	struct uthread_tcb *curr_thread_ptr = current_thread;
-	
-	//Context switch 
-	current_thread = next_thread;
-	current_thread->thread_state = RUNNING;
-	uthread_ctx_switch(curr_thread_ptr->context_ptr, next_thread->context_ptr);
+	if (queue_length(Ready_Queue) > 0) {
+		queue_dequeue(Ready_Queue, (void**)&next_thread);
+		struct uthread_tcb *curr_thread_ptr = current_thread;
+
+		//Context switch 
+		current_thread = next_thread;
+		current_thread->thread_state = RUNNING;
+		uthread_ctx_switch(curr_thread_ptr->context_ptr, 
+						   next_thread->context_ptr);
+	}
+
 	preempt_enable();	
 }
 
 void uthread_exit(void)
 {
 	preempt_disable();
+
 	//Make current thread "EXITED"
 	current_thread->thread_state = EXITED;
 	queue_enqueue(Exited_Queue, (void*)current_thread);
 	struct uthread_tcb *next_thread;
-	queue_dequeue(Ready_Queue, (void**)&next_thread);
-	struct uthread_tcb *current_thread_ptr = current_thread;
-	
-	//Context switch 
-	current_thread = next_thread;
-	current_thread->thread_state = 1;
-	uthread_ctx_switch(current_thread_ptr->context_ptr, next_thread->context_ptr);	
+
+	//Context switch to next available thread
+	if (queue_length(Ready_Queue) > 0) {
+		queue_dequeue(Ready_Queue, (void**)&next_thread);
+
+		struct uthread_tcb *current_thread_ptr = current_thread;
+		current_thread = next_thread;
+		current_thread->thread_state = RUNNING;
+		uthread_ctx_switch(current_thread_ptr->context_ptr, 
+						   next_thread->context_ptr);
+	}
+
 	preempt_enable();
 }
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-	struct uthread_tcb *new_thread = (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
-
+	struct uthread_tcb *new_thread = 
+			(struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
 	new_thread->stack_ptr = uthread_ctx_alloc_stack();
-	new_thread->context_ptr = (uthread_ctx_t *)malloc(sizeof(uthread_ctx_t)) ;
+	new_thread->context_ptr = 
+			(uthread_ctx_t *)malloc(sizeof(uthread_ctx_t)) ;
 	new_thread->thread_state = READY;
+
 	// check if context initialized
 	int retval = 0;
-	retval = uthread_ctx_init(new_thread->context_ptr, new_thread->stack_ptr, func, arg);
+	retval = uthread_ctx_init(new_thread->context_ptr, 
+							  new_thread->stack_ptr, func, arg);
 	if (new_thread->stack_ptr == NULL || retval == -1) return -1;
+
 	preempt_disable();
 	queue_enqueue(Ready_Queue, (void*)new_thread);
 	preempt_enable();
@@ -122,6 +137,7 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
 	//free the queue
 	clear_exited_queue();
 	queue_destroy(Ready_Queue);
+	Exited_Queue = NULL;
 	Ready_Queue = NULL;
 	return 0;
 }
@@ -130,15 +146,21 @@ void uthread_block(void)
 {
 	struct uthread_tcb *next_thread;
 	struct uthread_tcb *curr_thread_ptr = current_thread;
+
+	preempt_disable();
 	current_thread->thread_state = BLOCKED;
 	queue_dequeue(Ready_Queue, (void**)&next_thread);
 	current_thread = next_thread;
+	uthread_ctx_switch(curr_thread_ptr->context_ptr, 
+					   next_thread->context_ptr);
 	preempt_enable();
-	uthread_ctx_switch(curr_thread_ptr->context_ptr, next_thread->context_ptr);
 }
 
 void uthread_unblock(struct uthread_tcb *uthread)
 {
+	uthread->thread_state = READY;
+	preempt_disable();
 	queue_enqueue(Ready_Queue, (void*)uthread);
+	preempt_enable();
 }
 
