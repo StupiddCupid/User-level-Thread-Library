8ca3dfa1ac8f89336a67b7a3fda81326a6bc4b6b
Hugo <lminghao1@gmail.com>
2022-11-03 17:36:57 -0700

    add comments

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -8,37 +8,20 @@
 struct semaphore {
 	size_t count;
 	struct queue *blocked_queue;
-	/* TODO Phase 3 */
 };
 
-/*
- * sem_create - Create semaphore
- * @count: Semaphore count
- *
- * Allocate and initialize a semaphore of internal count @count.
- *
- * Return: Pointer to initialized semaphore. NULL in case of failure when
- * allocating the new semaphore.
- */
 sem_t sem_create(size_t count)
 {
 	struct semaphore *sem = (struct semaphore*) malloc(sizeof(struct semaphore));
 	if (sem == NULL) return NULL;
+
 	sem->blocked_queue = queue_create();
 	if (sem->blocked_queue == NULL) return NULL;
+
 	sem->count = count;
 	return sem;
 }
 
-/*
- * sem_destroy - Deallocate a semaphore
- * @sem: Semaphore to deallocate
- *
- * Deallocate semaphore @sem.
- *
- * Return: -1 if @sem is NULL or if other threads are still being blocked on
- * @sem. 0 is @sem was successfully destroyed.
- */
 int sem_destroy(sem_t sem)
 {
 	if (queue_length(sem->blocked_queue) != 0 || sem == NULL) return -1;
@@ -47,29 +30,20 @@ int sem_destroy(sem_t sem)
 	return 0;
 }
 
-/*
- * sem_down - Take a semaphore
- * @sem: Semaphore to take
- *
- * Take a resource from semaphore @sem.
- *
- * Taking an unavailable semaphore will cause the caller thread to be blocked
- * until the semaphore becomes available.
- *
- * Return: -1 if @sem is NULL. 0 if semaphore was successfully taken.
- */
 int sem_down(sem_t sem)
 {
-	// how to break out while loop
+	if (sem == NULL) return -1;
 	preempt_disable();
+	// if semaphore is not available, put thread into blocked queue
+	// then go to next available thread in ready queue
 	while (sem->count == 0) {
 		struct uthread_tcb *curr_thread;
 		curr_thread = uthread_current();
 		queue_enqueue(sem->blocked_queue, (void*)curr_thread);
 		uthread_block();
 	}
-	preempt_enable();
 	sem->count -= 1;
+	preempt_enable();
 	return 0;
 }
 
@@ -90,6 +64,7 @@ int sem_up(sem_t sem)
 	if (sem == NULL) return -1;
 	preempt_disable();
 	sem->count += 1;
+	// put a thread in blocked queue, if there is any, into ready queue
 	if (queue_length(sem->blocked_queue) != 0) {
 		struct uthread_tcb *uthread;
 		queue_dequeue(sem->blocked_queue, (void**)&uthread);
